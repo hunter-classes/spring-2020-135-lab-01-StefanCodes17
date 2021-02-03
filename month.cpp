faa346aa4466875225ff7341cbcbdc9a8d7060cc
Stefan Kolev <sk893q@gmail.com>
2021-02-02 19:54:12 -0500

    Updated month.cpp

--- month.cpp
+++ month.cpp
@@ -38,7 +38,7 @@ int main()
     {
         cout << "28";
     }
-    else if (month % 2 != 0)
+    else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
     {
         cout << "31";
     }
